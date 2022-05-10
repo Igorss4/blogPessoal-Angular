292ead2263b59db209b9eee261d6ef4f7d98c408
Igor Santos <igor.ox13@gmail.com>
2022-05-10 14:48:31 -0300

    crud de postagem

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-inicio',
@@ -9,7 +15,22 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class InicioComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  listaPostagens: Postagem[]
+  postagem: Postagem = new Postagem()
+
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema: number
+
+  usuario: Usuario = new Usuario()
+  idUsuario = environment.id
+
+  constructor(
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService,
+    private auth: AuthService
+  ) { }
 
   ngOnInit() {
     window.scroll(0, 0)
@@ -18,5 +39,47 @@ export class InicioComponent implements OnInit {
     //   alert('Voce precisa estar logado para ficar aqui...')
     //   this.router.navigate(['/entrar'])
     // }
+    this.getTemas()
+    this.getPostagens()
   }
+
+  getTemas() {
+    this.temaService.getTemas().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
   }
+
+  findByIdTema() {
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) => {
+      this.tema = resp
+    })
+  }
+
+  getPostagens(){
+    this.postagemService.getPostagem().subscribe((resp: Postagem[])=>{
+      this.listaPostagens = resp
+    })
+  }
+
+  findByIdUsuario(){
+    this.auth.getByIdUsuario(this.idUsuario).subscribe((resp: Usuario)=>{
+      this.usuario = resp
+    })
+  }
+
+  publicar() {
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+
+    this.usuario.id = this.idUsuario
+    this.postagem.usuario = this.usuario
+
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
+      this.postagem = resp
+      alert('Postagem realizada com sucesso!')
+      this.postagem = new Postagem()
+      this.getPostagens()
+    })
+  }
+
+}
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -22,6 +22,10 @@ export class AuthService {
       return this.http.post<Usuario>('http://localhost:8080/usuarios/cadastrar', usuario)
     }
 
+    getByIdUsuario(id: number): Observable<Usuario>{
+      return this.http.get<Usuario>(`http://localhost:8080/usuarios/${id}`)
+    }
+
     logado(){
       let ok: boolean = false;
 
